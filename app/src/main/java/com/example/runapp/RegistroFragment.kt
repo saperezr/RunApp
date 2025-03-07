78afbd9421cd56cd8f3232eb637e87812fe52b26
Sergio Perez <se.perezr12@uniandes.edu.co>
2025-03-06 21:55:41 -0500

    Crear Alarma

--- app/src/main/java/com/example/runapp/RegistroFragment.kt
+++ app/src/main/java/com/example/runapp/RegistroFragment.kt
@@ -5,8 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.navigation.fragment.findNavController
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -15,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [MenuFragment.newInstance] factory method to
+ * Use the [RegistroFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class MenuFragment : Fragment() {
+class RegistroFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -36,14 +34,7 @@ class MenuFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val root = inflater.inflate(R.layout.fragment_menu, container, false)
-
-        val btnAlarmas = root.findViewById<Button>(R.id.buttonAlarmas)
-
-        btnAlarmas.setOnClickListener{
-            findNavController().navigate(R.id.action_menuFragment_to_alarmasFragment)
-        }
-        return root
+        return inflater.inflate(R.layout.fragment_registro, container, false)
     }
 
     companion object {
@@ -53,12 +44,12 @@ class MenuFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment MenuFragment.
+         * @return A new instance of fragment RegistroFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            MenuFragment().apply {
+            RegistroFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
